e862e51195a17893d4e84dc70fcc8064cc262fb4
Tayo <teeydigba@gmail.com>
2025-08-18 23:34:20 +0100

    default ai/sdk chatbot

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
+import { useChat } from "@ai-sdk/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,7 +18,42 @@ export default function Home() {
     <div
       className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
     >
-     
+      <Chat />
     </div>
   );
 }
+
+export const Chat = () => {
+  const [input, setInput] = useState("");
+  const { messages, sendMessage } = useChat();
+  return (
+    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+      {messages.map((message) => (
+        <div key={message.id} className="whitespace-pre-wrap">
+          {message.role === "user" ? "User: " : "AI: "}
+          {message.parts.map((part, i) => {
+            switch (part.type) {
+              case "text":
+                return <div key={`${message.id}-${i}`}>{part.text}</div>;
+            }
+          })}
+        </div>
+      ))}
+
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          sendMessage({ text: input });
+          setInput("");
+        }}
+      >
+        <input
+          className="fixed dark:bg-zinc-900 bottom-0 w-full max-w-md p-2 mb-8 border border-zinc-300 dark:border-zinc-800 rounded shadow-xl"
+          value={input}
+          placeholder="Say something..."
+          onChange={(e) => setInput(e.currentTarget.value)}
+        />
+      </form>
+    </div>
+  );
+};
